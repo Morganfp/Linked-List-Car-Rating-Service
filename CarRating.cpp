35222f2cf5e6cc6ba127c262ca1764d12ad85dcf
Morganfp <109186494+Morganfp@users.noreply.github.com>
2022-07-17 18:26:35 -0400

    Update CarRating.cpp

--- CarRating.cpp
+++ CarRating.cpp
@@ -1,56 +1,60 @@
 /************************************************
- * FILE: CarRating.cpp                          *
- * AUTHOR: Dr. Blythe                           *
- * DATE: 2/2022                                 *
- * PURPOSE: Implements CarRating class          *
+ * FILE: CarRating.hpp                          *
+ * AUTHOR: Morgan Purcell                           *
+ * DATE: 04/19/2022                                 *
+ * PURPOSE: Header file for CarRating class     *
+ *   This class describes a car and a rating    *
+ *   for that car.                              *
  ************************************************/
-#include "CarRating.hpp"
-using namespace std;
-double
-CarRating::addNewRating(int rating)
+
+#ifndef _CAR_RATING_HPP_
+#define _CAR_RATING_HPP_
+
+#include <iostream>
+#include <string>
+
+//define the CarRating class
+class CarRating
 {
-  // if the rating is invalid, give a message indicating such and
-  // just return the current rating. 
-  if (rating<1 || rating>4)
-    {
-      cerr << "Error: rating value must be 1, 2, 3, or 4 ("
-   << rating << " is invalid)" << endl;
-      return _rating;
-    }
-  // a bit of math here to update the rating value. Ask Dr. Blythe if
-  //  you do not understand this math!
-  _rating = (_numRatings * _rating + rating)/(_numRatings+1);
-  _numRatings++;
-  return _rating; 
-}
-bool
-CarRating::operator==(const CarRating &other) const
-{
-  // remember that == doesnt care about the actual rating values, just the
-  // car make, model and year. 
-  return _year == other._year &&
-    _model == other._model &&
-    _make == other._make;
-}
-ostream&
-CarRating::print(ostream &os) const
-{
-  os << _year << " " << _make << " " << _model
-     << " " << _rating << " " << _numRatings << "";
-  return os;
-}
-// overloaded output operator ... just use the above print method!
-ostream& operator<< (ostream &os, const CarRating& cr)
-{
-  return cr.print(os);
-}
-//overloaded input operator ... yep, it's as simple as it looks!
-istream& operator>> (istream &is, CarRating& cr)
-{
-  is >> cr._year;
-  is >> cr._make;
-  is >> cr._model;
-  is >> cr._rating;
-  is >> cr._numRatings;
-  return is;
-}
+private:
+  std::string _make;   // the make (ex. Ford, Toyota, etc ...) for the car
+  std::string _model;  // the model (ex. Explorer, Camry, etc...) for the car
+  int _year;           // the model year for the car (ex. 1990, 2022, ...)
+  int _numRatings;     // number of ratings for this car
+  double _rating;      // current overall rating for this car. 
+
+public:
+  // default constructor
+  CarRating() :
+    _make("N/A"), _model("N/A"), _year(-1), _numRatings(0), _rating(0) {}
+
+  // constructor to build car out of make, model and year. 
+  CarRating(const std::string &make, const std::string &model, int year) :
+    _make(make), _model(model), _year(year), _numRatings(0), _rating(0) {}
+
+  // accessor methods 
+  const std::string& make() const {return _make;} 
+  const std::string& model() const {return _model;}
+  int year() const {return _year;}
+  int numRatings() const {return _numRatings;}
+  double rating() const {return _rating;}
+
+  // mutator methods for the rating and number of ratings.
+  int& numRatings() {return _numRatings;}
+  double& rating() {return _rating;}
+
+  // methods to add a new rating for a car
+  double addNewRating(int rating);
+  double operator+=(int newRating) {return addNewRating(newRating);}
+  
+  // compares two car ratings for equality
+  // two ratings are considered equal if they have the same make, model and year
+  bool operator==(const CarRating &other) const; 
+
+  // output a CarRating to the specified stream
+  std::ostream& print(std::ostream &os) const;
+  // overloaded >> (input) and << (output) operators
+  friend std::istream& operator>> (std::istream &is, CarRating& cr);
+  friend std::ostream& operator<< (std::ostream &os, const CarRating& cr);
+};
+#endif
